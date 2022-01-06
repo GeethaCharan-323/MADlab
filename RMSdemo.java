160e6fdd5d845d631f9ac7950be749c2f5262dc9
GeethaCharan-323 <59505708+GeethaCharan-323@users.noreply.github.com>
2022-01-06 08:33:29 -0800

    Add files via upload

--- RMSdemo.java
+++ RMSdemo.java
@@ -5,9 +5,10 @@ import javax.microedition.rms.*;
 public class RMSdemo extends MIDlet implements CommandListener{
 	private Display display;
 	private TextField name,score;
-	private Form form,res;
-	private Command save,top10,exit,del_rec;
-	private RecordStore rs=null;
+	private Form home,result;
+	private Command save,view,exit,back,del_rec;
+	
+	private RecordStore recordStore=null;
 	static final String REC_STORE = "RMSdemo";
 
 	public RMSdemo(){
@@ -16,30 +17,30 @@ public class RMSdemo extends MIDlet implements CommandListener{
 	public void startApp(){
 		display=Display.getDisplay(this);
 
-		form=new Form("RMS to store player score");
-		res=new Form("TOP10");
+		home=new Form("RMS to store player score");
+		
+		
 		name=new TextField("PlayerName : ","",30,TextField.ANY);
 		score=new TextField("Score : ","",30,TextField.ANY);
-		form.append(name);
-		form.append(score);
+		home.append(name);
+		home.append(score);
 
 		save=new Command("SAVE",Command.OK,1);
-		top10=new Command("TOP10",Command.OK,2);
-		del_rec=new Command("DELETE DATA",Command.OK,3);
+		view=new Command("VIEW",Command.OK,2);
+		del_rec=new Command("DELETE",Command.OK,3);
 		exit=new Command("EXIT",Command.EXIT,1);
-
-		form.addCommand(save);
-		form.addCommand(top10);
-		form.addCommand(del_rec);
-		form.addCommand(exit);
-		form.setCommandListener(this);
-
-		try{
-			rs = RecordStore.openRecordStore(REC_STORE, true );
-		}catch (Exception e){}
+		back=new Command("BACK",Command.EXIT,1);
 
 
-		display.setCurrent(form);
+		home.addCommand(save);
+		home.addCommand(view);
+		home.addCommand(del_rec);
+		home.addCommand(exit);
+		home.setCommandListener(this);
+
+		createRecordStore();
+
+		display.setCurrent(home);
 	}
 	public void pauseApp(){
 
@@ -50,42 +51,56 @@ public class RMSdemo extends MIDlet implements CommandListener{
 
 	public void commandAction(Command c,Displayable d){
 		if(c==save){
-			String temp=name.getString()+";"+score.getString();
+			String temp=name.getString()+" ; "+score.getString();
 			saveRecord(temp);
 		}
-		else if(c==top10){
-			displayTop10();
-		}
-		else if(c==del_rec){
-			deleteRecordStore();
+		else if(c==view){
+			displayRecords();
 		}
 		else if(c==exit){
 			destroyApp(false);
 		}
+		else if(c==back){
+			display.setCurrent(home);
+		}
+		else if(c==del_rec){
+			deleteRecordStore();
+		}
 	}
+
+
+	public void createRecordStore(){
+		try{
+			recordStore = RecordStore.openRecordStore(REC_STORE, true );
+		}catch (Exception e){}
+	}
+
+
 	public void saveRecord(String r){
 		byte[] record=r.getBytes();
 		try{
-			rs.addRecord(record,0,record.length);
-		}catch(Exception e){
-
-		}
+			recordStore.addRecord(record,0,record.length);
+		}catch(Exception e){}
 	}
-	public void displayTop10(){
+
+
+	public void displayRecords(){
 		try{
 			byte[] recData = new byte[5]; 
 			int len;
-
-			for(int i = 1; i <= rs.getNumRecords(); i++){
-				if(rs.getRecordSize(i) > recData.length){
-					recData = new byte[rs.getRecordSize(i)];
+			result=new Form("View Records");
+			for(int i = 1; i <= recordStore.getNumRecords(); i++){
+				if(recordStore.getRecordSize(i) > recData.length){
+					recData = new byte[recordStore.getRecordSize(i)];
 				}
-				len = rs.getRecord(i, recData, 0);
-				System.out.println("------------------------------");
-				System.out.println("Record " + i + " : " +
-					new String(recData, 0, len));
-				System.out.println("------------------------------");  
+				len = recordStore.getRecord(i, recData, 0);
+				String temp = new String(recData, 0, len);
+				temp=temp.concat("\n");
+				result.append(temp);  
 			}
+			result.addCommand(back);
+			result.setCommandListener(this);
+			display.setCurrent(result);
 		}catch (Exception e){}
 	}
 	public void deleteRecordStore(){
